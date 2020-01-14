a575ae0e248a2b4cba6c4e5bf9922ca86681919d
JeremyMarc232 <jeremymarc@hotmail.fr>
2020-01-14 12:07:58 +0100

    Ajout de 6 slides d'exo

---
+++ exo_05.rb
@@ -0,0 +1,28 @@
+puts "On va compter le nombre d'heures de travail à THP"
+puts "Travail : #{10 * 5 * 11}"
+#"10h/j * 5j/semaine * 11semaines"
+
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+#"multiplié par nb de minutes"
+
+puts "Et en secondes ?"
+puts 10 * 5 * 11 * 60 * 60
+#"multiplié par le nb de secondes"
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7
+#"ne pas mettre de guillemets permet d'effectuer l'addition"
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+#"permet d'intégrer valeur d'une variable dans une string"
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+#"même principe qu'au dessus"
